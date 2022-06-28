be5db0f13f621c446a2f86e4e370a2df5a26fba7
Supersamu <samuel.schueler@web.de>
2022-06-28 09:58:43 +0200

    fix after upgrading torch to 1.12.0+cu113

--- loss/supcon_loss.py
+++ loss/supcon_loss.py
@@ -22,9 +22,6 @@ class SupConLoss(nn.Module):
 
         # todo normalize both the teacher and the student output
         # question is normalize in what way
-        print(teacher_output.shape)
-        print(student_output.shape)
-        print(teacher_output.dtype)
-        print(student_output.dtype)
-        loss = self.loss_fn(input=teacher_output, target=torch.tensor(student_output.softmax(dim=1), dtype=torch.long))
+        
+        loss = self.loss_fn(input=teacher_output, target=student_output.softmax(dim=1))
         return loss
